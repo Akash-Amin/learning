6c52eb019d701978887e08932a2d0c49bb7afb61
Akash-Amin <6402amin@gmail.com>
2021-09-02 19:25:40 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-
-
+import ApiRequest from "./ApiRequest";
+//import Api from "./Api";
 
 function App() {
   return (
     <div className="App">
      <h1>hellooooo AKASH checking</h1>
+   <ApiRequest/>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,10 +1,40 @@
 const express = require("express");
+const mongoose = require ("mongoose");
 const app = express();
 
+
+const DB='mongodb+srv://dbuser:akash6402@mini.bclgg.mongodb.net/mern_blog?retryWrites=true&w=majority';
+//mongoose.connect(DB,)
+const middlware =(req,res,next)=>
+{
+    console.log("middleware");
+    next();
+}
+
+
 app.get("/", (req, res) => {
+    res.send("hellooo1");
+});
+
+app.get("/about",middlware, (req, res) => {
+    res.send("hellooo2");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("hellooo3");
+});
+
+app.get("/signin", (req, res) => {
     res.send("hellooo");
 });
 
-app.listen(5000,()=>{
-    console.log("sucess run")
-})
+app.get("/signup", (req, res) => {
+    res.send("hellooo");
+});
+
+app.listen(5000,(req,res)=>{
+    console.log("port connected");
+});
+
+
+
